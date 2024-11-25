c122fb60b49b0e8bdd79615d0dcdb11a0540db84
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2024-11-25 21:04:42 +0100

    Add a unit test for level 1-2

--- src/main/kotlin/Levels.kt
+++ src/main/kotlin/Levels.kt
@@ -2,9 +2,12 @@ package com.bartoszwesolowski
 
 import com.bartoszwesolowski.CarColor.Red
 import com.bartoszwesolowski.Direction.RIGHT
+import com.bartoszwesolowski.Direction.UP
 import com.bartoszwesolowski.Tile.Empty
 import com.bartoszwesolowski.Tile.EndingTrack
 import com.bartoszwesolowski.Tile.HorizontalTrack
+import com.bartoszwesolowski.Tile.Obstacle
+import com.bartoszwesolowski.Tile.VerticalTrack
 
 val board1_1 = Board(
     arrayOf(
@@ -14,4 +17,16 @@ val board1_1 = Board(
 
 val cars1_1 = arrayListOf(
     Car(1, Red, CarPosition(0, 0, RIGHT)),
+)
+
+val board1_2 = Board(
+    arrayOf(
+        arrayOf(Empty, Empty, Empty, Obstacle, Empty, EndingTrack),
+        arrayOf(Empty, Obstacle, Empty, Obstacle, Empty),
+        arrayOf(VerticalTrack, Obstacle, Empty, Empty, Empty, Obstacle),
+    )
+)
+
+val cars1_2 = arrayListOf(
+    Car(1, Red, CarPosition(2, 0, UP)),
 )
--- src/test/kotlin/SolverTest.kt
+++ src/test/kotlin/SolverTest.kt
@@ -1,7 +1,9 @@
 import com.bartoszwesolowski.Board
 import com.bartoszwesolowski.Solver
 import com.bartoszwesolowski.board1_1
+import com.bartoszwesolowski.board1_2
 import com.bartoszwesolowski.cars1_1
+import com.bartoszwesolowski.cars1_2
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import com.bartoszwesolowski.Tile.*
@@ -20,6 +22,22 @@ class SolverTest {
                 )
             ),
             solver.findSolutions(board1_1, cars1_1),
+        )
+    }
+
+    @Test
+    fun level1_2() {
+        assertEquals(
+            listOf(
+                Board(
+                    arrayOf(
+                        arrayOf(BottomRightTurn, HorizontalTrack, BottomLeftTurn, Obstacle, BottomRightTurn, EndingTrack),
+                        arrayOf(VerticalTrack, Obstacle, VerticalTrack, Obstacle, VerticalTrack, Obstacle),
+                        arrayOf(VerticalTrack, Obstacle, TopRightTurn, HorizontalTrack, TopLeftTurn, Obstacle),
+                    )
+                )
+            ),
+            solver.findSolutions(board1_2, cars1_2),
         )
     }
 }
