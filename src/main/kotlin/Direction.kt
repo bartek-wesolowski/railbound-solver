998513ecf7f90a2cb8c556052397f032992b0c3b
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2024-11-25 21:56:50 +0100

    Remove unused Direction.opposite()

--- src/main/kotlin/Direction.kt
+++ src/main/kotlin/Direction.kt
@@ -1,12 +1,5 @@
 package com.bartoszwesolowski
 
 enum class Direction {
-    UP, DOWN, LEFT, RIGHT;
-
-    fun opposite(): Direction = when (this) {
-        UP -> DOWN
-        DOWN -> UP
-        LEFT -> RIGHT
-        RIGHT -> LEFT
-    }
+    UP, DOWN, LEFT, RIGHT
 }
