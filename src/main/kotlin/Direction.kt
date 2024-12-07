f5b7b8b2e0654f28d171260983225200f4b3e549
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2024-12-07 17:03:15 +0100

    Remove Direction.opposite()

--- src/main/kotlin/Direction.kt
+++ src/main/kotlin/Direction.kt
@@ -1,13 +1,5 @@
 package com.bartoszwesolowski
 
 enum class Direction {
-    UP, DOWN, LEFT, RIGHT;
-
-    val opposite: Direction
-        get() = when (this) {
-            UP -> DOWN
-            DOWN -> UP
-            LEFT -> RIGHT
-            RIGHT -> LEFT
-        }
+    UP, DOWN, LEFT, RIGHT
 }
