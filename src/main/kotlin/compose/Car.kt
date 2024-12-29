e35491c528f14381fcee56aedc297e15283f6575
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2024-12-29 19:56:44 +0100

    Display car numbers and directions

--- src/main/kotlin/compose/Car.kt
+++ src/main/kotlin/compose/Car.kt
@@ -2,14 +2,19 @@ package compose
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import model.Car
 import model.CarColor
 import model.CarPosition
@@ -24,42 +29,62 @@ fun Car(
     tileSize: Dp,
     modifier: Modifier = Modifier,
 ) {
-    Canvas(modifier = modifier.size(tileSize)) {
-        val color = when (car.color) {
-            CarColor.Red -> Red
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.size(tileSize)
+    ) {
+        Canvas(modifier = Modifier.size(tileSize)) {
+            val color = when (car.color) {
+                CarColor.Red -> Red
+            }
+            val topLeft = when (car.direction) {
+                Direction.UP -> Offset(
+                    x = (tileSize.toPx() * (1 - carWidthPercent) / 2),
+                    y = (tileSize.toPx() * (1 - carHeightPercent))
+                )
+
+                Direction.DOWN -> Offset(
+                    x = (tileSize.toPx() * (1 - carWidthPercent) / 2),
+                    y = 0f
+                )
+
+                Direction.LEFT -> Offset(
+                    x = (tileSize.toPx() * (1 - carHeightPercent)),
+                    y = (tileSize.toPx() * (1 - carWidthPercent) / 2)
+                )
+
+                Direction.RIGHT -> Offset(
+                    x = 0f,
+                    y = (tileSize.toPx() * (1 - carWidthPercent) / 2)
+                )
+            }
+            val size = when (car.direction) {
+                Direction.UP, Direction.DOWN -> Size(
+                    width = tileSize.toPx() * carWidthPercent,
+                    height = tileSize.toPx() * carHeightPercent
+                )
+
+                Direction.LEFT, Direction.RIGHT -> Size(
+                    width = tileSize.toPx() * carHeightPercent,
+                    height = tileSize.toPx() * carWidthPercent
+                )
+            }
+            drawRect(
+                color = color,
+                topLeft = topLeft,
+                size = size
+            )
         }
-        val topLeft = when (car.direction) {
-            Direction.UP -> Offset(
-                x = (tileSize.toPx() * (1 - carWidthPercent) / 2),
-                y = (tileSize.toPx() * (1 - carHeightPercent))
-            )
-            Direction.DOWN -> Offset(
-                x = (tileSize.toPx() * (1 - carWidthPercent) / 2),
-                y = 0f
-            )
-            Direction.LEFT -> Offset(
-                x = (tileSize.toPx() * (1 - carHeightPercent)),
-                y = (tileSize.toPx() * (1 - carWidthPercent) / 2)
-            )
-            Direction.RIGHT -> Offset(
-                x = 0f,
-                y = (tileSize.toPx() * (1 - carWidthPercent) / 2)
-            )
+        val text = when (car.direction) {
+            Direction.UP, -> "^\n${car.number}"
+            Direction.DOWN -> "${car.number}\nv"
+            Direction.LEFT -> "<${car.number}"
+            Direction.RIGHT -> "${car.number}>"
         }
-        val size = when (car.direction) {
-            Direction.UP, Direction.DOWN -> Size(
-                width = tileSize.toPx() * carWidthPercent,
-                height = tileSize.toPx() * carHeightPercent
-            )
-            Direction.LEFT, Direction.RIGHT -> Size(
-                width = tileSize.toPx() * carHeightPercent,
-                height = tileSize.toPx() * carWidthPercent
-            )
-        }
-        drawRect(
-            color = color,
-            topLeft = topLeft,
-            size = size
+        Text(
+            text = text,
+            color = Color.White,
+            fontSize = 24.sp,
         )
     }
 }
