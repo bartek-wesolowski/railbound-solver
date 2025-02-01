f6955d252f48891c8e44ecece7fb541ebf25643e
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2025-02-01 22:34:16 +0100

    Convert Position to a value class

--- src/main/kotlin/model/Position.kt
+++ src/main/kotlin/model/Position.kt
@@ -1,6 +1,17 @@
 package model
 
-data class Position(
-    val row: Int,
+@JvmInline
+value class Position private constructor(
+    private val value: Int
+) {
+    constructor(row: Int, column: Int) : this(row * 32 + column) {
+        require(row >= 0 && column >= 0) { "Row and column must be non-negative" }
+        require(row < 32 && column < 32) { "Row and column must be less than 32" }
+    }
+
+    val row: Int
+        get() = value / 32
+
     val column: Int
-)
+        get() = value % 32
+}
