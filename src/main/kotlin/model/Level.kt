968ef05e9b820043fe55d7961c7b4d6fc2f517b2
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2025-02-16 14:54:27 +0100

    Add level preconditions verification

--- src/main/kotlin/model/Level.kt
+++ src/main/kotlin/model/Level.kt
@@ -1,6 +1,8 @@
 package model
 
 import com.danrusu.pods4k.immutableArrays.ImmutableArray
+import model.Tile.BaseHorizontalTrack.FixedHorizontalTrack
+import model.Tile.BaseVerticalTrack.FixedVerticalTrack
 
 data class Level(
     val name: String,
@@ -8,4 +10,24 @@ data class Level(
     val carColor: CarColor,
     val cars: ImmutableArray<Car>,
     val tracks: Int,
-)
+) {
+    init {
+        for (car in cars) {
+            require(car.row >= 0 && car.row < board.rows)
+            require(car.column >= 0 && car.column < board.columns)
+            val tile = board[car.row, car.column]
+            require(tile is FixedHorizontalTrack || tile is FixedVerticalTrack)
+            if (tile is FixedHorizontalTrack) {
+                require(car.direction == Direction.LEFT || car.direction == Direction.RIGHT)
+            } else {
+                require(car.direction == Direction.UP || car.direction == Direction.DOWN)
+            }
+            for (otherCar in cars) {
+                if (car != otherCar) {
+                    require(car.row != otherCar.row || car.column != otherCar.column)
+                    require(car.number != otherCar.number)
+                }
+            }
+        }
+    }
+}
