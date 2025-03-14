52369f743f872741733be7a7b13c4e9abd629c9a
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2025-03-14 12:36:35 +0100

    Remove custom equals and hashCode for SolverState

--- src/main/kotlin/solver/SolverState.kt
+++ src/main/kotlin/solver/SolverState.kt
@@ -25,22 +25,4 @@ data class SolverState(
     val carBreadcrumbs: CarBreadcrumbs,
 ) {
     fun createBreadcrumb() = Breadcrumb(activeCars, toggledColors, getInProgress)
-
-    private val hashCode: Int by lazy(LazyThreadSafetyMode.NONE) {
-        var result = board.hashCode()
-        result = 31 * result + activeCars.hashCode()
-        result
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is SolverState) return false
-
-        if (board != other.board) return false
-        if (activeCars != other.activeCars) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int = hashCode
 }
